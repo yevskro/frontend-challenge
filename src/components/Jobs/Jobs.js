c4bbcf1a35e641d3bea77143d2a23057e26a9f8c
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-01 17:37:11 -0400

    style: change to 2 spaces

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -2,17 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Jobs(){
-    return <StyledJobs role='main'>
+  return <StyledJobs role='main'>
 
-    </StyledJobs>
+  </StyledJobs>
 }
 
 const StyledJobs = styled.section`
-    position: absolute;
-    z-index: 2;
-    border: 1px solid red;
-    left: 5%;
-    right: 5%;
-    top: 120px;
+  position: absolute;
+  z-index: 2;
+  border: 1px solid red;
+  left: 5%;
+  right: 5%;
+  top: 120px;
 `
 export default Jobs;
