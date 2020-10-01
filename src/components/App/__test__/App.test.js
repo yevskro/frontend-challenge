23fab4f14cabc3e10f763e43de2887a24e9d9b73
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-01 14:51:35 -0400

    test(App): changed role

--- src/components/App/__test__/App.test.js
+++ src/components/App/__test__/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import '@testing-library/jest-dom/extend-expect';
 
 test('renders without crashing', () => {
   const { getByRole } = render(<App />);
-  const appElement = getByRole("app");
+  const appElement = getByRole("application");
   expect(appElement).toBeInTheDocument();
 });
