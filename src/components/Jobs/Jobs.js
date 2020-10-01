481b15b47d17dc76805d3a1fe5b833890ec40793
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-01 17:48:30 -0400

    feat(Jobs): add search styles and filter boilerplate

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 
 function Jobs(){
   return <StyledJobs role='main'>
-
+    <Search><Filter>CSS</Filter></Search>
+    <Job/>
+    <Job/>
+    <Job/>
   </StyledJobs>
 }
 
@@ -14,5 +17,23 @@ const StyledJobs = styled.section`
   left: 5%;
   right: 5%;
   top: 120px;
+`;
+
+const Filter = styled.div`
+
 `
+
+const Search = styled.div`
+  height: 72px;
+  border: 1px solid red;
+  display: flex;
+`;
+
+const Job = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  margin-top: 56px;
+`;
+
 export default Jobs;
