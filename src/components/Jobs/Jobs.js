7c6a8a677f8057f027799d05863200dcb371d930
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-01 17:36:35 -0400

    feat(Jobs): add z indexed positioning

---
+++ src/components/Jobs/Jobs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Jobs(){
+    return <StyledJobs role='main'>
+
+    </StyledJobs>
+}
+
+const StyledJobs = styled.section`
+    position: absolute;
+    z-index: 2;
+    border: 1px solid red;
+    left: 5%;
+    right: 5%;
+    top: 120px;
+`
+export default Jobs;
