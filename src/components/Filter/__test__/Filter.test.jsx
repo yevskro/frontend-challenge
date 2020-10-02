9bd0aea8e33bc73b0b34eddb15cad2915720fa8f
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 12:29:27 -0400

    style(Filter): test filter eslint prettier

---
+++ src/components/Filter/__test__/Filter.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Filter from '../Filter';
+
+test('renders without crashing', () => {
+  const { getByRole } = render(<Filter value='CSS' />);
+  const filterElement = getByRole('checkbox');
+  expect(filterElement).toBeInTheDocument();
+});
+
+test('renders props', () => {
+  const { getByText } = render(<Filter value='CSS' />);
+  const filterElement = getByText('CSS');
+  expect(filterElement).toBeInTheDocument();
+});
+
+test('snapshot', async () => {
+  const tree = renderer.create(<Filter />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
