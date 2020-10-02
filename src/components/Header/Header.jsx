56d27f14ea74bf7b337e35bcd97e8ac9857b0af8
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 12:27:26 -0400

    style(Header): eslint prettier

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <header role='presentation'>
+      <Image />
+    </header>
+  );
+}
+
+const Image = styled.img`
+  height: 156px;
+  background-color: #5da5a4;
+  background-image: url('/assets/images/bg-header-mobile.svg');
+  width: 100%;
+  @media (min-width: 376px) {
+    background-image: url('/assets/images/bg-header-desktop.svg');
+  }
+`;
+
+export default Header;
