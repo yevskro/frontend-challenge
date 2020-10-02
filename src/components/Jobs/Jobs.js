690d4e009e5a9ad4c46f01929a0d186d777adefe
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-01 20:30:21 -0400

    feat(Jobs): add font, box shadow

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Filters from '../Filters/Filters';
+import Filter from '../Filter/Filter';
 
 function Jobs(){
   return <StyledJobs role='main'>
-    <Search><Filter>CSS</Filter></Search>
+    <Search>
+      <Filters>
+        <Filter remove={true} value="Frontend"/>
+        <Filter remove={true} value="CSS"/>
+        <Filter remove={true} value="JavaScript"/>
+      </Filters>
+      <Clear/>
+    </Search>
     <Job/>
     <Job/>
     <Job/>
@@ -13,26 +22,35 @@ function Jobs(){
 const StyledJobs = styled.section`
   position: absolute;
   z-index: 2;
-  border: 1px solid red;
   left: 5%;
   right: 5%;
   top: 120px;
 `;
 
-const Filter = styled.div`
-
-`
-
 const Search = styled.div`
-  height: 72px;
-  border: 1px solid red;
   display: flex;
+  padding: 10px 25px 10px 10px;
+  align-items: center;
+  border-radius: 5px;
+  background-color: #FFF;
+  justify-content: space-between;
+  box-shadow: 0px 10px 10px RGBA(94, 140, 160, 0.15);
+  font-family: Spartan;
+  font-size: 13px;
+  font-weight: 600;
+`;
+
+const Clear = styled.span`
+  text-align: center;
+  &:after {
+    content: 'Clear';
+  }
+  color: #869290;
 `;
 
 const Job = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid red;
   margin-top: 56px;
 `;
 
