6b40c02a464d9a6fa2a9e9e1831d26a57ed95512
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 12:17:23 -0400

    feat(JobList): add bottom margin

---
+++ src/components/JobList/JobList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function JobList({ children }) {
+  return <StyledJobList>{children}</StyledJobList>;
+}
+
+const StyledJobList = styled.ul`
+  margin-bottom: 150px;
+`;
+export default JobList;
