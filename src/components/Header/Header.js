0e6d9baa004ca1f7363b79856318acfff220d930
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 07:10:09 -0400

    refactor(Header): change to a repeated background image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,24 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header(){
-    return <header role='presentation'>
-        <Image/>
-    </header>
+    return <header role='presentation'><Image/></header>
 }
 
 const Image = styled.img`
   height: 156px;
   background-color: #5DA5A4;
-  content: url('/assets/images/bg-header-mobile.svg');
+  background-image: url('/assets/images/bg-header-mobile.svg');
+  width: 100%;
   @media (min-width: 376px) {
-    content: url('/assets/images/bg-header-desktop.svg');
-  }
-  @media (min-width: 1367px) { 
-  /* 
-      the svg is actually 1366px in width, most browsers go beyond that width
-      and there will be white space on the right edge. this fills it
-  */
-    width: 100%;
+    background-image: url('/assets/images/bg-header-desktop.svg');
   }
 `;
 
