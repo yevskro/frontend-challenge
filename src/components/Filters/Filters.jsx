923e1b42d50ee4f4827aedc07fbbe8e0bb0e69d4
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 17:44:04 -0400

    feat(Filters): add responsiveness

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Filters({ children }) {
-  return <StyledFilters role='listbox'>{children}</StyledFilters>;
+function Filters({ children, flexEnd }) {
+  return <StyledFilters flexEnd={flexEnd} role='listbox'>{children}</StyledFilters>;
 }
 
 const StyledFilters = styled.ul`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
+  justify-content: ${(props) => props.flexEnd ? 'flex-end' : 'flex-start'};
+  @media (min-width: 1000px){
+      flex-wrap: nowrap;
+  }
+  @media (max-width: 415px){
+      justify-content: flex-start;
+  }
 `;
 
 export default Filters;
