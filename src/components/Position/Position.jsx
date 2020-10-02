d1c2aab1720ac7a0a4494803234ba68387ffb99c
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-02 17:42:28 -0400

    feat(Position): add responsiveness

--- src/components/Position/Position.jsx
+++ src/components/Position/Position.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import JobInfo from '../JobInfo/JobInfo';
+import Company from '../Company/Company';
 
-function Position({ children, postedAt, contract, location }) {
+function Position({ children, postedAt, contract, location, company }) {
   return (
     <StyledPosition>
-      <Children>{children}</Children>
+      <Company newly={company.newly} featured={company.featured}>{company.name}</Company>
+      <Heading>{children}</Heading>
       <JobInfo postedAt={postedAt} contract={contract} location={location} />
     </StyledPosition>
   );
 }
 
-const Children = styled.h3`
-  display: inline-block;
+const Heading = styled.h3`
   padding-top: 11px;
   padding-bottom: 11px;
   font-weight: 600;
   margin: 0;
+  white-space: nowrap;
 `;
 
-const StyledPosition = styled.div``;
+const StyledPosition = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default Position;
