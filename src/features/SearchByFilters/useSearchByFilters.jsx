b03ab032fc0ba0e6f2a81f4452e39111e67de115
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-04 12:12:14 -0400

    refactor: replaced getter for filters with reading filters directly

---
+++ src/features/SearchByFilters/useSearchByFilters.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+
+function useSearchByFilters(_data = []) {
+  /* simple search filter one level deep */
+  const [data] = useState(_data);
+  const [filters, setFilters] = useState([]);
+  const [filteredData, setFilteredData] = useState(data);
+
+  function filterData(aFilters) {
+    const newData = [];
+    let filtered = false;
+    for (let dIdx = 0; dIdx < data.length; dIdx += 1) {
+      for (let fIdx = 0; fIdx < aFilters.length; fIdx += 1) {
+        if (
+          typeof data[dIdx] === 'object' &&
+          typeof aFilters[fIdx] === 'object'
+        ) {
+          const filtersKey = Object.keys(aFilters[fIdx])[0];
+          const filterValue = aFilters[fIdx][filtersKey];
+          const dataValue = data[dIdx][filtersKey];
+          if (dataValue) {
+            if (Array.isArray(dataValue) && dataValue.includes(filterValue))
+              filtered = true;
+          }
+          if (
+            data[dIdx][filtersKey] &&
+            data[dIdx][filtersKey] === aFilters[fIdx][filtersKey]
+          )
+            filtered = true;
+        }
+      }
+      if (!filtered) {
+        newData.push(data[dIdx]);
+      } else filtered = false;
+    }
+    return newData;
+  }
+
+  function filterExists(filter, aFilters = filters, deleteIfExists = false) {
+    const filterKey =
+      typeof filter === 'object' ? Object.keys(filter)[0] : undefined;
+    const filterValue = filterKey ? filter[filterKey] : filter;
+
+    for (let idx = 0; idx < aFilters.length; idx += 1) {
+      if (filterKey) {
+        if (
+          typeof filters[idx] === 'object' &&
+          filters[idx][filterKey] &&
+          filters[idx][filterKey] === filterValue
+        ) {
+          if (deleteIfExists) {
+            aFilters.splice(idx, 1);
+          }
+          return true;
+        }
+      } else if (
+        typeof filters[idx] !== 'object' &&
+        filters[idx] === filterValue
+      ) {
+        if (deleteIfExists) {
+          aFilters.splice(idx, 1);
+        }
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function addFilter(filter) {
+    if (!filterExists(filter)) {
+      const newFilters = [...filters];
+      newFilters.push(filter);
+      setFilters(newFilters);
+      setFilteredData(filterData(newFilters));
+    }
+  }
+
+  function removeFilter(filter) {
+    const newFilters = [...filters];
+    filterExists(filter, newFilters, true);
+    setFilters(newFilters);
+    setFilteredData(filterData(newFilters));
+  }
+
+  return { filteredData, filters, addFilter, removeFilter };
+}
+
+export default useSearchByFilters;
