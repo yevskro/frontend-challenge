258cadcc735762e1d7a6f5e526aca90a1bde7d56
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 13:38:23 -0400

    test: fix theme access

--- src/shared/components/Header/__test__/Header.test.jsx
+++ src/shared/components/Header/__test__/Header.test.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import Theme from '../../../providers/Theme';
 import Header from '..';
 
-test('renders without crashing', () => {
-  const { getByRole } = render(<Header />);
+test('renders', () => {
+  const { getByRole } = render(
+    <Theme>
+      <Header />
+    </Theme>
+  );
   const appElement = getByRole('presentation');
   expect(appElement).toBeInTheDocument();
 });
 
 test('snapshot', async () => {
-  const tree = renderer.create(<Header />).toJSON();
+  const tree = renderer
+    .create(
+      <Theme>
+        <Header />
+      </Theme>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
