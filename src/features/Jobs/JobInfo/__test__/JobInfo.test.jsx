985dc829041f623f39c4bf77c2361d581ab0cb24
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 11:07:04 -0400

    test: change to using getByText

--- src/features/Jobs/JobInfo/__test__/JobInfo.test.jsx
+++ src/features/Jobs/JobInfo/__test__/JobInfo.test.jsx
@@ -1,49 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import Theme from '../../../../shared/providers/Theme';
 import JobInfo from '..';
-import Theme from '../../../providers/Theme';
 
-const testData = {
-  postedAt: '2d ago',
-  contract: 'Part Time',
-  location: 'USA Only',
-};
-
-test('renders without crashing', () => {
-  const { getByRole } = render(
+test('renders', () => {
+  render(
     <Theme>
-      <JobInfo
-        postedAt={testData.postedAt}
-        contract={testData.contract}
-        location={testData.location}
-      />
+      <JobInfo postedAt="2d ago" contract="Part Time" location="USA Only" />
     </Theme>
   );
 
-  const jobElement = getByRole('list');
-  expect(jobElement).toBeInTheDocument();
-});
-
-test('renders info elements', () => {
-  const { getAllByRole } = render(
-    <Theme>
-      <JobInfo
-        postedAt={testData.postedAt}
-        contract={testData.contract}
-        location={testData.location}
-      />
-    </Theme>
-  );
-
-  const infoElements = getAllByRole('listitem');
-  expect(infoElements.length).toBe(3);
-  expect(infoElements[0].textContent).toStrictEqual(testData.postedAt);
-  expect(infoElements[0]).toBeInTheDocument();
-  expect(infoElements[1].textContent).toStrictEqual(testData.contract);
-  expect(infoElements[1]).toBeInTheDocument();
-  expect(infoElements[2].textContent).toStrictEqual(testData.location);
-  expect(infoElements[2]).toBeInTheDocument();
+  expect(screen.getByText('2d ago')).toBeInTheDocument();
+  expect(screen.getByText('Part Time')).toBeInTheDocument();
+  expect(screen.getByText('USA Only')).toBeInTheDocument();
 });
 
 test('snapshot', async () => {
