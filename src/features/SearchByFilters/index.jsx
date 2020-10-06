be81fdd5164804e89d2171cd9def85374e151690
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 10:47:39 -0400

    refactor: changed min-height added commentary

--- src/features/SearchByFilters/index.jsx
+++ src/features/SearchByFilters/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import SearchClearButton from './SearchClearButton';
 
-function SearchByFilters({ children, visible, onClear }) {
+/* 
+  search by filters children should consist of SearchFilters which gives the developer
+  choice to flexend or flexstart its children
+*/
+function SearchByFilters({ children, visible, onClearFilters }) {
   return (
     <StyledSearchByFilters visible={visible} role="search">
       {children}
-      <SearchClearButton onClick={onClear} />
+      <SearchClearButton onClick={onClearFilters} />
     </StyledSearchByFilters>
   );
 }
@@ -15,13 +19,13 @@ const StyledSearchByFilters = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  min-height: 75px;
-  padding: 10px 25px 10px 21px;
+  min-height: 65px;
+  padding: 5px 26px 5px 21px;
   margin-bottom: 56px;
   border-radius: 5px;
   background-color: #fff;
   box-shadow: 0px 10px 10px ${({ theme }) => theme.color.shadow};
-  visibility: ${({ visible }) => (visible ? 'visibile' : 'hidden')};
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
 `;
 
 export default SearchByFilters;
