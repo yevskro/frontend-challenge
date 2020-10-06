33ae05080ab58a97fba6651c4f0c9ce3b33587c9
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 11:11:25 -0400

    test: refactor to using getByText()

--- src/features/Jobs/Job/__test__/Job.test.jsx
+++ src/features/Jobs/Job/__test__/Job.test.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import jobData from '../../../../shared/test/test-data.json';
+import Theme from '../../../../shared/providers/Theme';
 import Job from '..';
-import Theme from '../../../providers/Theme';
-import jobData from '../../../data.json';
 
-test('renders without crashing', () => {
-  const { debug, getByRole } = render(
+/*
+    "id": 1,
+    "company": "Photosnap",
+    "logo": "/assets/images/photosnap.svg",
+    "newly": true,
+    "featured": false,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "postedAt": "2d ago",
+    "contract": "Part Time",
+    "location": "USA Only",
+    "languages": ["HTML", "CSS", "JavaScript"],
+    "tools": []
+*/
+test('renders', () => {
+  render(
     <Theme>
-      <Job data={[jobData[0]]} />
+      <Job data={jobData[0]} />
     </Theme>
   );
-  debug();
-  const jobElement = getByRole('article');
-  expect(jobElement).toBeInTheDocument();
+  expect(screen.getByText('Photosnap')).toBeInTheDocument();
+  expect(screen.getByRole('img', { name: 'company logo' })).toHaveAttribute(
+    'src',
+    '/assets/images/photosnap.svg'
+  );
+  expect(screen.getByRole('img', { name: 'company logo' })).toBeInTheDocument();
+  expect(screen.getByText('NEW!')).toBeInTheDocument();
+  expect(screen.getByText('Senior Frontend Developer')).toBeInTheDocument();
+  expect(screen.getByText('Frontend')).toBeInTheDocument();
+  expect(screen.getByText('Senior')).toBeInTheDocument();
+  expect(screen.getByText('2d ago')).toBeInTheDocument();
+  expect(screen.getByText('Part Time')).toBeInTheDocument();
+  expect(screen.getByText('USA Only')).toBeInTheDocument();
+  expect(screen.getByText('HTML')).toBeInTheDocument();
+  expect(screen.getByText('CSS')).toBeInTheDocument();
+  expect(screen.getByText('JavaScript')).toBeInTheDocument();
 });
 
 test('snapshot', async () => {
   const tree = renderer
     .create(
       <Theme>
-        <Job data={[jobData[0]]} />
+        <Job data={jobData[0]} />
       </Theme>
     )
     .toJSON();
