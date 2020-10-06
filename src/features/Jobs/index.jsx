ba7e7e14d707c4e1686b4c5ffb98fad6b7f2cf2d
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 10:58:50 -0400

    refactor: using dispatchers with the search reducer and generating jsx components through functions

--- src/features/Jobs/index.jsx
+++ src/features/Jobs/index.jsx
@@ -1,43 +1,81 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
 import SearchByFilters from '../SearchByFilters';
 import Job from './Job';
 import JobList from './JobList';
-import jobData from '../../data.json';
 import SearchFilters from '../SearchByFilters/SearchFilters';
 import SearchFilter from '../SearchByFilters/SearchFilter';
-import useSearchByFilters from '../../shared/hooks/useSearchByFilters';
+import searchByFiltersReducer from '../../shared/reducers/searchByFiltersReducer';
+import {
+  removeFilter,
+  addFilter,
+  clearFilters,
+  filterData,
+} from '../../shared/reducers/searchByFiltersReducer/actionCreators';
 
-function Jobs() {
-  const {
-    filteredData,
-    filters,
-    addFilter,
-    removeFilter,
-    clearFilters,
-  } = useSearchByFilters(jobData);
+function Jobs({ jobs }) {
+  /* filters the job media objects specific to users filters */
+  const [search, dispatch] = useReducer(searchByFiltersReducer, { data: jobs });
 
-  const jsxFilters = filters.map((filter) => (
-    <SearchFilter filter={filter} remove onRemoveFilter={removeFilter}>
-      {filter[Object.keys(filter)[0]]}
-    </SearchFilter>
-  ));
+  function onRemoveFilter(filter) {
+    dispatch(removeFilter(filter));
+    dispatch(filterData());
+  }
 
-  const jsxJobs = filteredData.map((job) => (
-    <Job data={job} onAddFilter={addFilter} />
-  ));
+  function onAddFilter(filter) {
+    dispatch(addFilter(filter));
+    dispatch(filterData());
+  }
+
+  function onClearFilters() {
+    dispatch(clearFilters());
+    dispatch(filterData());
+  }
+
+  function jsxFilters() {
+    /* generate filters based on search filters */
+    const filters =
+      search.filters ||
+      []; /* search.filters may be undefined if not initalized */
+    return filters.map((filter) => {
+      const value = filter[Object.keys(filter)[0]];
+      return (
+        <SearchFilter
+          filter={filter}
+          remove
+          onRemoveFilter={onRemoveFilter}
+          key={`search-filter-${value}`}
+        >
+          {value}
+        </SearchFilter>
+      );
+    });
+  }
+
+  function jsxJobs() {
+    /* generate Job components based on search data */
+    const jobsData =
+      search.data || []; /* search data may be undefined if not initialized */
+    return jobsData.map((job) => (
+      <Job data={job} onAddFilter={onAddFilter} key={`job-media-${job.id}`} />
+    ));
+  }
 
   return (
     <StyledJobs role="main">
-      <SearchByFilters visible={!!filters.length} onClear={clearFilters}>
-        <SearchFilters>{jsxFilters}</SearchFilters>
+      <SearchByFilters
+        visible={search.filters && search.filters.length !== 0}
+        onClearFilters={onClearFilters}
+      >
+        <SearchFilters>{jsxFilters()}</SearchFilters>
       </SearchByFilters>
-      <JobList>{jsxJobs}</JobList>
+      <JobList>{jsxJobs()}</JobList>
     </StyledJobs>
   );
 }
 
 const StyledJobs = styled.section`
+  /* this will stack to the front */
   position: absolute;
   z-index: 2;
   left: 5%;
@@ -46,6 +84,7 @@ const StyledJobs = styled.section`
   background: transparent;
 
   @media (min-width: 401px) {
+    /* gives it a tighter feel */
     left: 11%;
     right: 11%;
   }
