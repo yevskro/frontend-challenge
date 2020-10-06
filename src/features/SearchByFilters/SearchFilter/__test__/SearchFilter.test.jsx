0d7e416b4355019764244294be1f9425632f044d
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-06 10:52:55 -0400

    test: add remove, remove filter icon, invokation of remove and add filter

--- src/features/SearchByFilters/SearchFilter/__test__/SearchFilter.test.jsx
+++ src/features/SearchByFilters/SearchFilter/__test__/SearchFilter.test.jsx
@@ -1,21 +1,82 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderer from 'react-test-renderer';
+import Theme from '../../../../shared/providers/Theme';
 import SearchFilter from '..';
 
-test('renders without crashing', () => {
-  const { getByRole } = render(<SearchFilter value="CSS" />);
-  const filterElement = getByRole('checkbox');
-  expect(filterElement).toBeInTheDocument();
+test('renders', () => {
+  render(
+    <Theme>
+      <SearchFilter>CSS</SearchFilter>
+    </Theme>
+  );
+  expect(screen.getByText('CSS')).toBeInTheDocument();
 });
 
-test('renders props', () => {
-  const { getByText } = render(<SearchFilter value="CSS" />);
-  const filterElement = getByText('CSS');
-  expect(filterElement).toBeInTheDocument();
+test('onAddFilter callback invoked', () => {
+  const mock = jest.fn((filter) =>
+    expect(filter).toStrictEqual({ test: 'test' })
+  );
+
+  render(
+    <Theme>
+      <SearchFilter onAddFilter={mock} filter={{ test: 'test' }}>
+        CSS
+      </SearchFilter>
+    </Theme>
+  );
+
+  userEvent.click(screen.getByText('CSS'));
+  expect(mock).toHaveBeenCalledTimes(1);
+});
+
+test('renders a remove filter with a remove icon', () => {
+  render(
+    <Theme>
+      <SearchFilter value="CSS" remove />
+    </Theme>
+  );
+
+  const img = screen.getByRole('img');
+  expect(img).toHaveAttribute('src', '/assets/images/icon-remove.svg');
+  expect(img).toBeInTheDocument();
+});
+
+test('onRemoveFilter callback invoked', () => {
+  const mock = jest.fn((filter) =>
+    expect(filter).toStrictEqual({ test: 'test' })
+  );
+
+  render(
+    <Theme>
+      <SearchFilter
+        value="CSS"
+        remove
+        onRemoveFilter={mock}
+        filter={{ test: 'test' }}
+      />
+    </Theme>
+  );
+
+  const img = screen.getByRole('img');
+  userEvent.click(img);
+  expect(mock).toHaveBeenCalledTimes(1);
 });
 
 test('snapshot', async () => {
-  const tree = renderer.create(<SearchFilter />).toJSON();
+  const mock = jest.fn();
+  const tree = renderer
+    .create(
+      <Theme>
+        <SearchFilter filter={{ test: 'test' }} onAddFilter={mock}>
+          CSS
+        </SearchFilter>
+        <SearchFilter filter={{ test: 'test' }} remove onRemoveFilter={mock}>
+          HTML
+        </SearchFilter>
+      </Theme>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
